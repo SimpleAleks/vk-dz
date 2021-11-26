cdceaa8e0c3f8b71e3575857fe54ae20b69e9d19
Ilyaa Alexandrov <pukisona12@yandex.ru>
2021-11-26 09:50:03 +0300

    Add Spring Boot start project on Gradle

---
+++ src/main/java/com/vkdz/vkdz/VkdzApplication.java
@@ -0,0 +1,13 @@
+package com.vkdz.vkdz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkdzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkdzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vkdz/vkdz/VkdzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkdz.vkdz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkdzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
